35bf967320809c905e66584aeb59ff65672087bf
AndrewKamSki <104537278+AndrewKamSki@users.noreply.github.com>
2022-09-26 12:08:07 -0400

    Added write to file function. Issue with populateHTML getting Role of employees

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-// Imports
+// Import html helper code
+const populateHTML = require('./src/populateHTML');
+
+// Class Imports
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
@@ -131,25 +134,18 @@ const addTeamMembers = () => {
   })
 }
 
+// Write HTML file
+const writeToFile = data => {
+  fs.writeFile('./dist/index.html', data, (err) => {
+    err ? console.log(err) : console.log('Team profile created successfully in index.html file')
+  })
+}
+
 createTeam()
   .then(checkAddEmployee)
-
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//   fs.writeFile(fileName, data, (err) =>
-//       err ? console.log(err) : console.log('Successfully created README.md')
-//   );
-// }
-
-// // TODO: Create a function to initialize app
-// function init() {
-//   inquirer
-//     .prompt([...questions])
-//     .then((answers) => {
-//       writeToFile('README.md',generateMarkdown({...answers}));
-//     })
-// }
-
-// // Function call to initialize app
-// init();
+  .then(team => {
+    return populateHTML(team);
+  })
+  .then(htmlPage => {
+    return writeToFile(htmlPage);
+  })
