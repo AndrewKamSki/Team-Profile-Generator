6329a76fe4a19a32a80133361db5ba683914ce69
AndrewKamSki <104537278+AndrewKamSki@users.noreply.github.com>
2022-09-26 05:43:29 -0400

    Employee test completed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,6 +1,6 @@
 const Employee = require('../lib/Employee');
 
-describe("Employee", () => {
+fdescribe("Employee", () => {
     describe("Initialization", () => {
         it("create an object with a name, id, and email if provided vaild arguments",() => {
             const employee = new Employee('Andy', 20, 'andy.kaminski42@gmail.com');
@@ -8,6 +8,30 @@ describe("Employee", () => {
             expect(employee.name).toEqual("Andy");
             expect(employee.id).toEqual(20);
             expect(employee.email).toEqual("andy.kaminski42@gmail.com");
-        })
-    })
-})
+        });
+
+        it("retrieve an employee name",() => {
+            const employee = new Employee('Andy', 20, 'andy.kaminski42@gmail.com');
+
+            expect(employee.getName()).toEqual("Andy");
+        });
+
+        it("retrieve an employee id",() => {
+            const employee = new Employee('Andy', 20, 'andy.kaminski42@gmail.com');
+
+            expect(employee.getID()).toEqual(20);
+        });
+
+        it("retrieve an employee name",() => {
+            const employee = new Employee('Andy', 20, 'andy.kaminski42@gmail.com');
+
+            expect(employee.getEmail()).toEqual("andy.kaminski42@gmail.com");
+        });
+
+        it("retrieve a role",() => {
+            const employee = new Employee('Andy', 20, 'andy.kaminski42@gmail.com');
+
+            expect(employee.getRole()).toEqual("Employee");
+        });
+    });
+});
